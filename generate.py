faa258a19d231d9b3fd46f29bc3a8e19fe151f33
negat <negat@microsoft.com>
2016-11-11 00:38:03 +0000

    generate works with no options

--- generate.py
+++ generate.py
@@ -1,8 +1,34 @@
 import argparse
 import json
+import sys
 
 parser = argparse.ArgumentParser(description="Generate VMSS Quickstart Templates.")
-parser.add_argument('--osType', help='semver package version', required=True, choices=['Windows', 'Linux', 'Both'])
+parser.add_argument("--osType", required=True, choices=["Windows", "Linux", "Both"], type=str)
+parser.add_argument("--outFilePath", required=False, type=str)
+parser.add_argument("--debug", required=False, type=bool)
 args = parser.parse_args()
 
-print(args.osType)
+outFilePath = "out/out.json"
+if args.outFilePath:
+    outFilePath = args.outFilePath
+
+with open("templates/linuxScaleSet.json", "r") as linuxScaleSetFile:
+    res = json.loads(linuxScaleSetFile.read())
+
+if args.osType == "Windows":
+    print("TODO")
+    sys.exit()
+elif args.osType == "Linux":
+    ""
+elif args.osType == "Both":
+    print("TODO")
+    sys.exit()
+else:
+    print("Invalid osType. Not generating template.")
+    sys.exit()
+
+if args.debug:
+    print(res)
+else:
+    with open(outFilePath, "w") as outFile:
+        outFile.write(json.dumps(res))
