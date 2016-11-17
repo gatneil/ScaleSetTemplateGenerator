e36206e50c0ea1fcc27ab9761b6d7ccc3564f1aa
negat <negat@microsoft.com>
2016-11-17 19:39:22 +0000

    added vnetType option (existing or new)

--- generate.py
+++ generate.py
@@ -2,6 +2,8 @@ import argparse
 import json
 import sys
 
+from collections import OrderedDict
+
 def getListEntryIndex(k, v, l):
     i = 0
     for elem in l:
@@ -15,23 +17,25 @@ def getListEntryIndex(k, v, l):
 
 parser = argparse.ArgumentParser(description="Generate VMSS Quickstart Templates.")
 parser.add_argument("--osType", required=True, choices=["Windows", "Linux", "Both"], type=str)
-parser.add_argument("--outFilePath", required=False, type=str)
-parser.add_argument("--debug", required=False, type=bool)
+parser.add_argument("--outFilePath", required=False, type=str, default="out/out.json")
+parser.add_argument("--debug", required=False, type=bool, choices=[True, False], default=False)
+parser.add_argument("--vnetType", required=False, choices=["New", "Existing"], type=str, default="New")
 args = parser.parse_args()
 
-outFilePath = "out/out.json"
-if args.outFilePath:
-    outFilePath = args.outFilePath
-
 with open("templates/linuxScaleSet.json", "r") as linuxScaleSetFile:
-    res = json.loads(linuxScaleSetFile.read())
+    res = json.loads(linuxScaleSetFile.read(), object_pairs_hook=OrderedDict)
 
+with open("templates/vnet.json", "r") as vnetFile:
+    vnetDict = json.loads(vnetFile.read(), object_pairs_hook=OrderedDict)
+
+
+# customize OS Type
 if args.osType == "Windows":
     with open("templates/windowsImageReference.json", "r") as windowsImageReferenceFile:
         windowsImageReference = json.loads(windowsImageReferenceFile.read())
 
     vmssResourceIndex = getListEntryIndex("type", "Microsoft.Compute/virtualMachineScaleSets", res["resources"])
-    res["resources"][vmssResourceIndex]["properties"]["virtualMachineProfile"]["imageReference"] = windowsImageReference
+    res["resources"][vmssResourceIndex]["properties"]["virtualMachineProfile"]["storageProfile"]["imageReference"] = windowsImageReference
 
 elif args.osType == "Linux":
     ""
@@ -42,10 +46,39 @@ else:
     print("Invalid osType. Not generating template.")
     sys.exit()
 
+
+# customize new or existing VNET
+if args.vnetType == "New":
+    res["variables"]["vnetName"] = "vnet"
+    res["variables"]["subnetName"] = "subnet"
+    res["variables"]["vnetID"] = "[resourceId('Microsoft.Network/virtualNetworks',variables('vnetName'))]"
+    res["variables"]["subnetRef"] = "[concat(variables('vnetID'),'/subnets/',variables('subnetName'))]"
+    
+    vmssResourceIndex = getListEntryIndex("type", "Microsoft.Compute/virtualMachineScaleSets", res["resources"])
+    res["resources"][vmssResourceIndex]["properties"]["virtualMachineProfile"]["networkProfile"]["networkInterfaceConfigurations"][0]["properties"]["ipConfigurations"][0]["properties"]["subnet"]["id"] = "[variables('subnetRef')]"
+    res["resources"][vmssResourceIndex]["dependsOn"].append("[concat('Microsoft.Network/virtualNetworks/', variables('vnetName'))]")
+
+    res["resources"].append(vnetDict["resource"])
+    
+
+    
+elif args.vnetType == "Existing":
+    res["parameters"]["existingSubnetResourceId"] = vnetDict["existingSubnetParameterBlock"]
+    
+    vmssResourceIndex = getListEntryIndex("type", "Microsoft.Compute/virtualMachineScaleSets", res["resources"])
+    res["resources"][vmssResourceIndex]["properties"]["virtualMachineProfile"]["networkProfile"]["networkInterfaceConfigurations"][0]["properties"]["ipConfigurations"][0]["properties"]["subnet"]["id"] = "[parameters('existingSubnetResourceId')]"
+    
+else:
+    print("Invalid value for vnetType. Not generating template.")
+    sys.exit()
+
+
+# print out result or write it to file
 if args.debug:
+    print(args.outFilePath)
     print(res)
 else:
-    with open(outFilePath, "w") as outFile:
+    with open(args.outFilePath, "w") as outFile:
         outFile.write(json.dumps(res))
 
 
